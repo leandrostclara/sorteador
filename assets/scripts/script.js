0d5a624e8e8c932a67312ae598abad86d5284557
Leandro Santa Clara <leandrostclara@hotmail.com>
2023-12-01 16:03:05 -0400

    add condition to require max number higher than min number

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,6 +3,7 @@ const drawBtn = document.querySelector(".draw-btn")
 
 function sortearNumero() {
   
+    const imgSorteio = document.querySelector(".img-sorteio")
     const containerNumero = document.querySelector(".numero-sorteado-container")
     const numeroSorteado = document.querySelector(".numero-sorteado")
 
@@ -11,8 +12,16 @@ function sortearNumero() {
     
     const result = Math.floor(Math.random() * (max - min + 1) + min)
    
-    containerNumero.style.display=("block")
-    numeroSorteado.innerHTML = result
+    if (max > min) {
+
+        containerNumero.style.display=("block")
+        numeroSorteado.innerHTML = result
+
+    } else {
+
+        alert ("O numero maximo não pode ser menor que o mínimo !")
+
+    }
 
 }
 
