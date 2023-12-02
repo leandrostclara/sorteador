6b85fd84a1a89bb7fd41343fde92b31cf3f779ea
Leandro Santa Clara <leandrostclara@hotmail.com>
2023-12-02 01:22:31 -0400

    increment count

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,6 @@ const drawBtn = document.querySelector(".draw-btn")
 
 function sortearNumero() {
   
-    const imgSorteio = document.querySelector(".img-sorteio")
     const containerNumero = document.querySelector(".numero-sorteado-container")
     const numeroSorteado = document.querySelector(".numero-sorteado")
 
@@ -13,9 +12,36 @@ function sortearNumero() {
     const result = Math.floor(Math.random() * (max - min + 1) + min)
    
     if (max > min) {
+      
+        setTimeout(function() {
 
-        containerNumero.style.display=("block")
-        numeroSorteado.innerHTML = result
+            containerNumero.style.display=("block")
+            numeroSorteado.style.fontSize=("30px")
+            numeroSorteado.innerHTML = "SORTEANDO"
+
+        },1000)
+        setTimeout(function() {
+
+            numeroSorteado.innerHTML = "SORTEANDO."
+
+        },2000)
+        setTimeout(function() {
+
+            numeroSorteado.innerHTML = "SORTEANDO.."
+
+        },3000)
+        setTimeout(function() {
+
+            numeroSorteado.innerHTML = "SORTEANDO..."
+
+        },4000)
+        setTimeout(function() {
+
+            numeroSorteado.style.fontSize=("50px")
+            numeroSorteado.innerHTML = result
+
+        },5000)
+
 
     } else {
 
